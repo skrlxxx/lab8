1bf7e224cc15bf8d7aa99a1709d9a84477744bac
Сатулина Анна <annasatulina@yandex.ru>
2024-10-05 08:21:06 +0300

    update

---
+++ time.pas
@@ -0,0 +1,12 @@
+﻿program task02;
+begin
+  var n:= readinteger;
+  if ((n mod 4) = 0) and ((n mod 100) <> 0) and ((n mod 400) <> 0) then
+    println('366')
+  else
+    println('365')
+end.
+{2024
+366}
+{2023
+365}
